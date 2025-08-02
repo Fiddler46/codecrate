f2933e1d4a3fd145c77b1cddd673e73731d87b4b
Fiddler46 <adhyaksh99@gmail.com>
2025-08-02 18:29:40 +0530

    Schema and layout of initial db ui

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma"
+import GitHub from "next-auth/providers/github"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
+  ],
+  session: { strategy: 'database' },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: { signIn: '/login' }
+})
