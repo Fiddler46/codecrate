c8250f9a346858c2c70c7e9953d0e2d9413a00e4
Fiddler46 <adhyaksh99@gmail.com>
2026-07-28 18:32:38 +0530

    Fixed reloading issue on next

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // allowedDevOrigins: ["http://localhost:3000", "b4bb9115-4688-40f3-9972-96c0316daff8-00-2hovsp9rdmwp4.pike.replit.dev"],
+    webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/.git/**',
+        '**/node_modules/**',
+        '**/.next/**',
+        '**/.cache/**',
+        '**/.replit',
+        '**/.config/**',
+        '**/.upm/**',
+        '**/.agents/**',
+        '**/.local/**',
+        '**/attached_assets/**',
+      ],
+    }
+    return config
+  },
+  allowedDevOrigins: ["http://localhost:3000", "b4bb9115-4688-40f3-9972-96c0316daff8-00-2hovsp9rdmwp4.pike.replit.dev"],
 };
 
 export default nextConfig;
